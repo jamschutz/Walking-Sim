bff5d298963c0bc4a4696f4ace5a81f509ecbad9
jamschutz <52084893+jamschutz@users.noreply.github.com>
2021-10-20 13:54:47 -0400

    dialogController maybe working? who can say?

--- Assets/Joey/Scripts/Dialog/DialogController.cs
+++ Assets/Joey/Scripts/Dialog/DialogController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace jsch
 {
@@ -15,11 +16,83 @@ namespace jsch
     }
 
     [RequireComponent(typeof(DialogAudio))]
+    [RequireComponent(typeof(Collider))]
     public class DialogController : MonoBehaviour
     {
+    // ------ PUBLIC VARS -------------------------------------- //
         public Dialog[] dialog;
-        [Range(0.01f, 1)]
+
+        [Range(0.001f, 1)]
         public float typingSpeed;
+
+        public bool onlyShowOnce = true;
+
+
+
+    // ------ PRIVATE VARS -------------------------------------- //
+        private DialogAudio dialogAudio;
+        int currentDialogIndex;
+        TMP_Text textMeshPro;
+        
+        const float maxTimeBetweenTypingChars = 1.001f;
+        float timeBetweenTypingChars;
+
+
+
+    // ------ LIFECYCLE FUNCTIONS -------------------------------------- //
+        void Start()
+        {
+            dialogAudio = GetComponent<DialogAudio>();
+            textMeshPro = GetComponent<TMP_Text>();
+            currentDialogIndex = 0;
+            timeBetweenTypingChars = maxTimeBetweenTypingChars - typingSpeed;
+        }
+
+
+        void OnTriggerEnter(Collider other)
+        {
+            // only show dialog if the player enters trigger box
+            if(other.tag == "Player") {
+                StartDialog();
+
+                // if we only want to show once, destroy the collider
+                // so that it doesn't trigger again
+                if(onlyShowOnce) {
+                    var collider = GetComponent<Collider>();
+                    Destroy(collider);
+                }
+            }
+        }
+
+
+
+    
+    // ------ HELPER FUNCTIONS -------------------------------------- //
+        void StartDialog()
+        {
+            ShowNextDialog();
+        }
+
+
+        void ShowNextDialog()
+        {
+            textMeshPro.text = "";
+        }
+
+
+        IEnumerator TypeDialogText()
+        {
+            // get current text
+            string text = dialog[currentDialogIndex].text;
+            float waitTime = dialog[currentDialogIndex].waitTime;
+            textMeshPro.text = "";
+
+            // show character one by one, pausing between characters
+            for(int i = 0; i < text.Length; i++) {
+                textMeshPro.text += text[i];
+                yield return new WaitForSeconds(timeBetweenTypingChars);
+            }            
+        } 
 
     }
 }
